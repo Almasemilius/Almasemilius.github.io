a999cf1caabc6f63b003546f0b96f43b4cb72c46
almasemilius <almasemilius@gmail.com>
2023-04-23 01:27:38 +0300

    contact form feedback popup

---
+++ src/assets/svgs/tick.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Tick() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 96 960 960"
+      className="h-20 w-20"
+    >
+      <path d="M378 810 154 586l43-43 181 181 384-384 43 43-427 427Z" />
+    </svg>
+  );
+}
